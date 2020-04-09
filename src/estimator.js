42c50b1641fb139dcb95f8e8855ec3d0fd8f7fa0
Mukuria-Samuel <snmukuria@gmail.com>
2020-04-09 22:49:17 +0300

    Updated impact and severe functions

--- src/estimator.js
+++ src/estimator.js
@@ -13,18 +13,30 @@ const input_data = {
 }
 
 const covid19ImpactEstimator = (data) => data;
-const output = {
+let output = {
 	data: {},
 	impact: {},
 	severeImpact: {}
 };
-output.data = input_data;
+//output.data = input_data;
 var currentlyInfected = input_data.reportedCases*10;
+output.data='currentlyInfected'
 output.impact = currentlyInfected;
-//console.log(output)
+console.log(output)
 
 var severelyInfected = input_data.reportedCases*50;
+output.data='severelyInfected: '
 output.severeImpact= severelyInfected;
+console.log(output)
+
+var infectionsByRequestedTime = input_data.reportedCases*10*(2**10);
+output.data='infectionsByRequestedTime'
+output.impact = infectionsByRequestedTime;
+console.log(output)
+
+var infectionsByRequestedTime = input_data.reportedCases*50*(2**10);
+output.data='severeInfectionsByRequestedTime: '
+output.severeImpact = infectionsByRequestedTime;
 console.log(output)
 
 //export default covid19ImpactEstimator; added module.export due to a module error
