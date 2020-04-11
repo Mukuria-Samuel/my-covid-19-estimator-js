105e72b9509f44b1f542b71e03cfefad78b53d58
Mukuria-Samuel <snmukuria@gmail.com>
2020-04-11 20:52:54 +0300

    changes to file to correct indentation form tabs to spaces

--- src/estimator.js
+++ src/estimator.js
@@ -29,48 +29,49 @@ const periodNormaliser = (currentlyInfected, days, data) => {
     return currentlyInfected * (2 ** decimalDiscard);
 };
 
-const covid19ImpactEstimator = (data) => data;
-const days = data.timeToElapse;
-const currentlyInfected = data.reportedCases*10;
+const covid19ImpactEstimator = (data) => data {
+    const days = data.timeToElapse;
+    const currentlyInfected = data.reportedCases*10;
 
-output.impact.currentlyInfected = currentlyInfected;
-output.severeImpact.currentlyInfected = data.reportedCases*50;
+    output.impact.currentlyInfected = currentlyInfected;
+    output.severeImpact.currentlyInfected = data.reportedCases*50;
 
-const infectionsByRequestedTime = currentlyInfected;
-output.impact.infectionsByRequestedTime = periodNormaliser(currentlyInfected,days,data);
-output.severeImpact.infectionsByRequestedTime = periodNormaliser((currentlyInfected)*50,days,data);
+    const infectionsByRequestedTime = currentlyInfected;
+    output.impact.infectionsByRequestedTime = periodNormaliser(currentlyInfected,days,data);
+    output.severeImpact.infectionsByRequestedTime = periodNormaliser((currentlyInfected)*50,days,data);
 
-/****************** CH_2 ************************/
-const severeCasesByRequestedTime = currentlyInfected;
-let severeCases = periodNormaliser(currentlyInfected,days,data)*(15/100);
-output.impact.severeCasesByRequestedTime = severeCases;
-output.severeImpact.severeCasesByRequestedTime = severeCases*50;
+    /****************** CH_2 ************************/
+    const severeCasesByRequestedTime = currentlyInfected;
+    let severeCases = periodNormaliser(currentlyInfected,days,data)*(15/100);
+    output.impact.severeCasesByRequestedTime = severeCases;
+    output.severeImpact.severeCasesByRequestedTime = severeCases*50;
 
-const hospitalBedsByRequestedTime = severeCasesByRequestedTime;
-let bedsTotal = data.totalHospitalBeds;
-let bedsAvailable = ((35/100) - bedsTotal)| 0;
-output.impact.hospitalBedsByRequestedTime = severeCases - bedsAvailable;
-output.severeImpact.hospitalBedsByRequestedTime = (severeCases*50) - bedsAvailable;
+    const hospitalBedsByRequestedTime = severeCasesByRequestedTime;
+    let bedsTotal = data.totalHospitalBeds;
+    let bedsAvailable = ((35/100) - bedsTotal)| 0;
+    output.impact.hospitalBedsByRequestedTime = severeCases - bedsAvailable;
+    output.severeImpact.hospitalBedsByRequestedTime = (severeCases*50) - bedsAvailable;
 
-/****************** CH_3 ************************/
+    /****************** CH_3 ************************/
 
-const casesForICUByRequestedTime = infectionsByRequestedTime;
-let casesIcu = periodNormaliser(currentlyInfected,days,data);
-output.impact.casesForICUByRequestedTime = (5/100) * casesIcu;
-output.severeImpact.casesForICUByRequestedTime = (5/100) * casesIcu;
+    const casesForICUByRequestedTime = infectionsByRequestedTime;
+    let casesIcu = periodNormaliser(currentlyInfected,days,data);
+    output.impact.casesForICUByRequestedTime = (5/100) * casesIcu;
+    output.severeImpact.casesForICUByRequestedTime = (5/100) * casesIcu;
 
-const casesForVentilatorsByRequestedTime = infectionsByRequestedTime;
-let casesVentilator = periodNormaliser(currentlyInfected,days,data);
-output.impact.casesForVentilatorsByRequestedTime = ((2/100) * casesVentilator) | 0;
-output.severeImpact.casesForVentilatorsByRequestedTime = ((2/100) * casesVentilator*50) | 0;
+    const casesForVentilatorsByRequestedTime = infectionsByRequestedTime;
+    let casesVentilator = periodNormaliser(currentlyInfected,days,data);
+    output.impact.casesForVentilatorsByRequestedTime = ((2/100) * casesVentilator) | 0;
+    output.severeImpact.casesForVentilatorsByRequestedTime = ((2/100) * casesVentilator*50) | 0;
 
-const dollarsInFlight = infectionsByRequestedTime;
-let infectionByTime = periodNormaliser(currentlyInfected,days,data);
-output.impact.dollarsInFlight = ((infectionByTime * 0.65 * 1.5) / 30) | 0;
-output.severeImpact.dollarsInFlight = (((infectionByTime*50) * 0.65 * 1.5) / 30) | 0;
+    const dollarsInFlight = infectionsByRequestedTime;
+    let infectionByTime = periodNormaliser(currentlyInfected,days,data);
+    output.impact.dollarsInFlight = ((infectionByTime * 0.65 * 1.5) / 30) | 0;
+    output.severeImpact.dollarsInFlight = (((infectionByTime*50) * 0.65 * 1.5) / 30) | 0;
 
-//export default covid19ImpactEstimator; added module.export due to a module error
-output.data = data;
+    //export default covid19ImpactEstimator; added module.export due to a module error
+    output.data = data;
     return output;
+};
 module.exports = covid19ImpactEstimator;
 console.log(output)
